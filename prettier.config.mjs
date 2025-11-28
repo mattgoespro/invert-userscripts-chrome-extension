5c1578511f6a98a7a03c6e367351c71691e0c483
Matt Young <31349794+mattgoespro@users.noreply.github.com>
2025-11-28 19:41:53 +0200

    cp

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,9 +1,9 @@
 /** @type { import("prettier").Config } */
 export default {
   semi: true,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   singleQuote: true,
   printWidth: 100,
   tabWidth: 2,
-  useTabs: false
-}
+  useTabs: false,
+};
